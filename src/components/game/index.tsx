136578979dea7071e69e88c073cd0330c00715fc
Jorge Rubiano <ostricajh@gmail.com>
2025-12-02 20:34:16 +0000

    02: Base components

--- src/components/game/index.tsx
+++ src/components/game/index.tsx
@@ -1,7 +1,9 @@
 import { GameState } from "../../logic";
 import { PlayerId } from "rune-sdk";
 import { useEffect, useState } from "react";
-import { GameWrapper } from "./components";
+import { Bubble, Checkmark, GameWrapper, Grid } from "./components";
+
+const tmpCOlor = "BLUE";
 
 const Game = () => {
   const [game, setGame] = useState<GameState>();
@@ -26,8 +28,18 @@ const Game = () => {
   console.log(yourPlayerId);
 
   return (
-    <GameWrapper>
-      <div style={{ color: "white" }}>Inicia el Juego!!</div>
+    <GameWrapper currentColor={tmpCOlor}>
+      <Grid>
+        <Bubble
+          isPop={false}
+          isSelected={false}
+          isDisabled={false}
+          color="PURPLE"
+          position={{ row: 0, col: 0 }}
+          onClick={(data) => console.log(data)}
+        />
+      </Grid>
+      <Checkmark color={tmpCOlor} onClick={() => console.log("ACEPTA")} />
     </GameWrapper>
   );
 };
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,13 +1,30 @@
 export const BASE_WIDTH = 412;
 export const BASE_HEIGHT = 732;
 export const BASE_CLASS_NAME_GAME = "game";
+export const SIZE_GRID = Math.round(BASE_WIDTH * 0.89);
+export const GAP_GRID = 5;
+export const TOTAL_CELLS = 6;
+export const TOTAL_CELLS_GRID = 6 ** 2;
+export const SIZE_CELL = Math.ceil(SIZE_GRID / TOTAL_CELLS);
 
 export enum EBoardColor {
   BLUE = "BLUE",
   RED = "RED",
 }
 
+export enum EBubbleColors {
+  PURPLE = "PURPLE",
+  BLUE = "BLUE",
+  GREEN = "GREEN",
+  YELLOW = "YELLOW",
+  ORANGE = "ORANGE",
+  RED = "RED",
+}
+
 export const JS_CSS_VARIABLES: Record<string, number> = {
   BASE_WIDTH,
   BASE_HEIGHT,
+  SIZE_GRID,
+  GAP_GRID,
+  SIZE_CELL,
 };
